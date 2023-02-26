8ba63ecaa645585303623c95f49fdfe9991248fe
Vincent Vic <vic.coding@gmail.com>
2023-02-26 19:43:31 +0800

    init rd

---
+++ start/src/main/java/cn/meshed/cloud/rd/ProviderApplication.java
@@ -0,0 +1,18 @@
+package cn.meshed.cloud.rd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <h1>启动类</h1>
+ *
+ * @author Vincent Vic
+ * @version 1.0
+ */
+@SpringBootApplication(scanBasePackages = {"cn.meshed.cloud", "com.alibaba.cola"})
+public class ProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderApplication.class, args);
+    }
+}
