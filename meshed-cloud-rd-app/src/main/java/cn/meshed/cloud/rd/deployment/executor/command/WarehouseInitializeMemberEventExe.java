c3a694030c771b9f4d008df41f6b55fa153490bd
Vincent Vic <vic.coding@gmail.com>
2023-05-11 13:59:42 +0800

    fix: 修复成员业务错误

--- meshed-cloud-rd-app/src/main/java/cn/meshed/cloud/rd/deployment/executor/command/WarehouseInitializeMemberEventExe.java
+++ meshed-cloud-rd-app/src/main/java/cn/meshed/cloud/rd/deployment/executor/command/WarehouseInitializeMemberEventExe.java
@@ -4,8 +4,6 @@ import cn.meshed.cloud.cqrs.EventExecute;
 import cn.meshed.cloud.rd.deployment.event.WarehouseInitializeEvent;
 import cn.meshed.cloud.rd.domain.log.Trend;
 import cn.meshed.cloud.rd.domain.project.gateway.MemberGateway;
-import cn.meshed.cloud.rd.project.command.ProjectMemberCmd;
-import cn.meshed.cloud.rd.project.enums.ProjectRoleEnum;
 import cn.meshed.cloud.rd.project.executor.command.ProjectMemberCmdExe;
 import cn.meshed.cloud.utils.ResultUtils;
 import com.alibaba.cola.dto.Response;
@@ -13,8 +11,6 @@ import com.alibaba.fastjson.JSONObject;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-
-import java.util.Collections;
 
 /**
  * <h1>仓库成员初始化</h1>
@@ -40,15 +36,16 @@ public class WarehouseInitializeMemberEventExe implements EventExecute<Warehouse
     @Override
     public Response execute(WarehouseInitializeEvent warehouseInitializeEvent) {
         log.info("仓库初始化事件【仓库成员初始化消费者】: {}", JSONObject.toJSONString(warehouseInitializeEvent));
-        ProjectMemberCmd projectMemberCmd = new ProjectMemberCmd();
-        projectMemberCmd.setProjectKey(warehouseInitializeEvent.getProjectKey());
-        projectMemberCmd.setProjectRole(ProjectRoleEnum.ADMIN);
-        Long currentUid = Long.valueOf(warehouseInitializeEvent.getOperator().getId());
-        Integer mid = memberGateway.queryIdByUid(currentUid);
-        if (mid == null) {
-            return ResultUtils.fail("成员不存在，系统存在权限风险");
-        }
-        projectMemberCmd.setMemberIds(Collections.singletonList(mid));
-        return projectMemberCmdExe.execute(projectMemberCmd);
+//        ProjectMemberCmd projectMemberCmd = new ProjectMemberCmd();
+//        projectMemberCmd.setProjectKey(warehouseInitializeEvent.getProjectKey());
+//        projectMemberCmd.setProjectRole(ProjectRoleEnum.ADMIN);
+//        Long currentUid = Long.valueOf(warehouseInitializeEvent.getOperator().getId());
+//        Integer mid = memberGateway.queryIdByUid(currentUid);
+//        if (mid == null) {
+//            return ResultUtils.fail("成员不存在，系统存在权限风险");
+//        }
+//        projectMemberCmd.setMemberIds(Collections.singletonList(mid));
+//        return projectMemberCmdExe.execute(projectMemberCmd);
+        return ResultUtils.ok();
     }
 }
